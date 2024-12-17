7a17bd776129e982334ead5098448d0efe911b9b
sarah <sarah.benamor@esprit.tn>
2024-12-17 15:38:20 +0100

    integration

---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+use App\Repository\ServiceRepository;
+use App\Repository\PersonnelleRepository;
+use App\Entity\Service;
+use App\Entity\Personnelle;
+use App\Repository\FoyerRepository;
+use App\Repository\ChambreRepository;
+use App\Repository\DemandeSelectionRepository;
+use App\Repository\TrajetRepository;
+use App\Repository\VehiculeRepository;
+use App\Entity\DemandeSelection;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UtilisateurController extends AbstractController
+{
+    #[Route('/utilisateur', name: 'app_utilisateur')]
+    public function index(FoyerRepository $foyerRepository, ChambreRepository $chambreRepository,TrajetRepository $trajetRepository, VehiculeRepository $vehiculeRepository,ServiceRepository $ServiceRepository): Response
+    {
+        $foyers = $foyerRepository->findAll();
+        $chambres = $chambreRepository->findAll();
+
+        $trajets = $trajetRepository->findAll();
+        $vehicules = $vehiculeRepository->findAll();
+
+        $services = $ServiceRepository->findAll();
+
+        return $this->render('utilisateur.html.twig', [
+            'foyers' => $foyers,
+            'chambres' => $chambres,
+            'trajets' => $trajets,
+            'vehicules' => $vehicules,
+            'services'=> $services
+        ]);
+    }
+
+    #[Route('/utilisateur/selectionner-chambre', name: 'app_selectionner_chambre', methods: ['POST'])]
+    public function selectionnerChambre(
+        Request $request,
+        ChambreRepository $chambreRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $chambreId = $request->request->get('chambre_id');
+        
+        $chambre = $chambreRepository->find($chambreId);
+
+        if (!$chambre) {
+            $this->addFlash('error', 'Chambre non trouvée.');
+            return $this->redirectToRoute('app_utilisateur');
+        }
+
+        $foyer = $chambre->getFoyer();
+
+        if (!$foyer) {
+            $this->addFlash('error', 'Le foyer associé à cette chambre n\'est pas trouvé.');
+            return $this->redirectToRoute('app_utilisateur');
+        }
+
+
+
+        $demande = new DemandeSelection();
+        $demande->setChambre($chambre);
+        $demande->setFoyer($foyer); 
+        $demande->setDateDemande(new \DateTime());
+        $demande->setStatut('En attente');
+
+        $entityManager->persist($demande);
+        $entityManager->flush();
+
+        $this->addFlash('successs', 'Votre demande de sélection a été soumise avec succès.');
+
+        return $this->redirectToRoute('app_utilisateur');
+    }
+    
+       
+}
